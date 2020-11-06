730334cfd2bac930d248f1d4d754c4bd9b349e88
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-11-06 13:08:22 +0100

    Implemented PlaneGenerator.

---
+++ Editor/PlaneGenerator.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit
+{
+    public class PlaneGenerator
+    {
+        private Vector3 center;
+        private float length;
+        private float width;
+
+        public PlaneGenerator(Vector3 center, float length, float width)
+        {
+            this.center = center;
+            this.length = length;
+            this.width = width;
+        }
+
+        public IEnumerable<Vector3> Vertices
+        {
+            get
+            {
+                var upperLeft = center + new Vector3(-length / 2, 0, width / 2);
+                yield return upperLeft;
+
+                var upperRight = center + new Vector3(length / 2, 0, width / 2);
+                yield return upperRight;
+
+                var lowerLeft = center + new Vector3(-length / 2, 0, -width / 2);
+                yield return lowerLeft;
+
+                yield return lowerLeft;
+                yield return upperRight;
+
+                var lowerRight = center + new Vector3(length / 2, 0, -width / 2);
+                yield return lowerRight;
+            }
+        }
+
+        public IEnumerable<int> Triangles
+        {
+            get
+            {
+                var verticesCount = 6;
+                for (int i = 0; i < verticesCount; i++)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Editor/UnitTests/PlaneGeneratorTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using ProceduralToolkit;
+
+namespace UnitTests
+{
+    public class PlaneGeneratorTests
+    {
+        private PlaneGenerator plane;
+
+        [SetUp]
+        public void SetUp()
+        {
+            plane = new PlaneGenerator(Vector3.zero, 2, 1);
+        }
+
+        [Test]
+        public void TestForCorrectVertices()
+        {
+            var expectedVertices = new Vector3[]
+            {
+                new Vector3(-1, 0, 0.5f),
+                new Vector3(1, 0, 0.5f),
+                new Vector3(-1, 0, -0.5f),
+                new Vector3(-1, 0, -0.5f),
+                new Vector3(1, 0, 0.5f),
+                new Vector3(1, 0, -0.5f)
+            };
+            CollectionAssert.AreEqual(expectedVertices, plane.Vertices);
+        }
+
+        [Test]
+        public void TestForCorrectTriangles()
+        {
+            var expectedTriangles = new int[] { 0, 1, 2, 3, 4, 5 };
+            CollectionAssert.AreEqual(expectedTriangles, plane.Triangles);
+        }
+    }
+}
