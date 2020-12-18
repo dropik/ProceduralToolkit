ce19cd2471044b10119cc392caead5918ae6be00
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-18 10:57:28 +0100

    ListField delegates size change to ListSizeField

--- Editor/UI/ListField.cs
+++ Editor/UI/ListField.cs
@@ -1,21 +1,19 @@
 ﻿using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 using UnityEngine;
 
 namespace ProceduralToolkit.UI
 {
-    public class ListField : BaseField<List<Object>>
+    public class ListField : BaseField<List<Object>>, IListField
     {
         private Foldout foldout;
-        private IntegerField sizeField;
 
+        public IntegerField SizeField { get; set; }
         public IListElementFactory ElementFactory { get; set; }
         public System.Type ObjectType { get; set; }
 
         public ListField() : this("") { }
-
         public ListField(string label) : base(label, null)
         {
             InitList();
@@ -33,8 +31,7 @@ namespace ProceduralToolkit.UI
         private void CreateHierarchy()
         {
             InitFoldout();
-            InitSizeField();
-            foldout.Add(sizeField);
+            foldout.Add(SizeField);
             Add(foldout);
         }
 
@@ -46,81 +43,16 @@ namespace ProceduralToolkit.UI
             };
         }
 
-        private void InitSizeField()
+        public void AddElement()
         {
-            sizeField = new IntegerField()
-            {
-                name = "size",
-                label = "Size",
-                value = 0
-            };
-            sizeField.RegisterValueChangedCallback(OnSizeChanged);
-        }
-
-        private void OnSizeChanged(ChangeEvent<int> e)
-        {
-            if (NewValueIsNegative(e))
-            {
-                RestorePreviousValue(e);
-                return;
-            }
-            HandlePositiveValue(e);
-        }
-
-        private bool NewValueIsNegative(ChangeEvent<int> e)
-        {
-            return e.newValue < 0;
-        }
-
-        private void RestorePreviousValue(ChangeEvent<int> e)
-        {
-            sizeField.SetValueWithoutNotify(e.previousValue);
-        }
-
-        private void HandlePositiveValue(ChangeEvent<int> e)
-        {
-            if (IncrementedValue(e))
-            {
-                AddElements(e);
-            }
-            else if (DecrementedValue(e))
-            {
-                RemoveElements(e);
-            }
-        }
-
-        private bool IncrementedValue(ChangeEvent<int> e)
-        {
-            return e.newValue > e.previousValue;
-        }
-
-        private void AddElements(ChangeEvent<int> e)
-        {
-            value.Capacity = e.newValue;
-            for (int i = e.previousValue; i < e.newValue; i++)
-            {
-                AddNewElementWithId(i);
-            }
-        }
-
-        private void AddNewElementWithId(int id)
-        {
-            // Just allocate space for an eventual object.
-            // Actual object will be passed via ObjectField value change callback.
             value.Add(null);
-
-            var newElement = InitNewElement(id);
+            var id = value.Count - 1;
+            var newElement = ElementFactory.CreateElement(id);
             foldout.Add(newElement);
-
             if (id > 0)
             {
                 CopyPreviousElement(id, newElement);
             }
-        }
-
-        private ObjectField InitNewElement(int id)
-        {
-            return ElementFactory.CreateElement(id);
         }
 
         private void CopyPreviousElement(int id, ObjectField newElement)
@@ -129,24 +61,18 @@ namespace ProceduralToolkit.UI
             newElement.value = prevElement.value;
         }
 
-        private bool DecrementedValue(ChangeEvent<int> e)
+        public void RemoveElement()
         {
-            return e.newValue < e.previousValue;
-        }
-
-        private void RemoveElements(ChangeEvent<int> e)
-        {
-            for (int i = e.previousValue - 1; i >= e.newValue; i--)
-            {
-                RemoveElementAtId(i);
-            }
-        }
-
-        private void RemoveElementAtId(int id)
-        {
+            var id = value.Count - 1;
             var element = this.Query<ObjectField>($"element{id}").First();
             foldout.Remove(element);
             value.RemoveAt(id);
+        }
+
+        public void UpdateValueAt(int id)
+        {
+            var updatedElement = this.Query<ObjectField>($"element{id}").First();
+            value[id] = updatedElement.value;
         }
     }
 }
--- Tests/Editor/UITests/ListFieldTest.cs
+++ Tests/Editor/UITests/ListFieldTest.cs
@@ -1,6 +1,5 @@
 ﻿using NUnit.Framework;
 using UnityEngine;
-using UnityEditor;
 using ProceduralToolkit.UI;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
@@ -11,7 +10,6 @@ namespace ProceduralToolkit.EditorTests.UITests
     public class ListFieldTest : BaseCustomVETest
     {
         private ListField listField;
-        private IntegerField sizeField;
         private Mock<IListElementFactory> mockElementFactory;
 
         private const string TEST_OBJ_NAME = "Some Object";
@@ -28,7 +26,6 @@ namespace ProceduralToolkit.EditorTests.UITests
                 ElementFactory = mockElementFactory.Object,
                 ObjectType = typeof(ScriptableObject)
             };
-            sizeField = listField.Query<IntegerField>("size").First();
 
             return listField;
         }
@@ -47,36 +44,6 @@ namespace ProceduralToolkit.EditorTests.UITests
             Assert.That(foldout.value, Is.False);
         }
 
-        [Test]
-        public void TestHasOnlyOneIntegerField()
-        {
-            var sizeFields = listField.Query<IntegerField>().ToList();
-            Assert.That(sizeFields.Count, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void TestSizeField()
-        {
-            Assert.That(sizeField, Is.Not.Null);
-            Assert.That(sizeField.label, Is.EqualTo("Size"));
-            Assert.That(sizeField.value, Is.EqualTo(0));
-        }
-
-        [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        [TestCase(3)]
-        public void TestCreatedElementsCountWhileIncrementingSizeField(int newSize)
-        {
-            SetupMockToReturnObjects(newSize);
-
-            sizeField.value = newSize;
-
-            var elements = listField.Query<ObjectField>().ToList();
-            Assert.That(elements.Count, Is.EqualTo(newSize));
-            mockElementFactory.Verify(mock => mock.CreateElement(It.IsAny<int>()), Times.Exactly(newSize));
-        }
-
         private void SetupMockToReturnObjects(int objectsToCreate)
         {
             for (int i = 0; i < objectsToCreate; i++)
@@ -88,92 +55,92 @@ namespace ProceduralToolkit.EditorTests.UITests
         }
 
         [Test]
-        [TestCase(1)]
-        [TestCase(2)]
-        public void TestNewElementWhenShouldBeNull(int elementsToCreate)
+        public void TestNewValueIsAddedToListOnAddElement()
         {
-            SetupMockToReturnObjects(elementsToCreate);
-
-            sizeField.value = elementsToCreate;
-
-            for (int i = 0; i < elementsToCreate; i++)
-            {
-                var elementI = listField.Query<ObjectField>($"element{i}").First();
-                Assert.That(elementI.value, Is.Null);
-            }
+            listField.AddElement();
+            Assert.That(listField.value.Count, Is.EqualTo(1));
         }
 
         [Test]
-        public void TestNewElementWhenShouldNotBeNull()
+        public void TestNewElementIsAddedToHierarchyOnAddElement()
         {
-            SetupMockToReturnObjects(2);
-            SetupFirstElement();
-
-            sizeField.value++;
-
-            var element1 = listField.Query<ObjectField>("element1").First();
-            Assert.That(element1.value, Is.Not.Null);
-            Assert.That(element1.value.name, Is.EqualTo(TEST_OBJ_NAME));
+            SetupMockToReturnObjects(1);
+            listField.AddElement();
+            mockElementFactory.Verify(
+                mock => mock.CreateElement(It.Is<int>(id => id == 0)),
+                Times.Once);
+            var objectFieldsFound = listField.Query<ObjectField>().ToList();
+            Assert.That(objectFieldsFound.Count, Is.EqualTo(1));
         }
 
-        private void SetupFirstElement()
+        [Test]
+        public void TestPreviousElementCopiedIfExists()
         {
-            sizeField.value = 1;
+            SetupMockToReturnObjects(2);
+            listField.AddElement();
             var element0 = listField.Query<ObjectField>("element0").First();
             var testObject = ScriptableObject.CreateInstance<ScriptableObject>();
             testObject.name = TEST_OBJ_NAME;
             element0.value = testObject;
+
+            listField.AddElement();
+
+            var element1 = listField.Query<ObjectField>("element1").First();
+            Assert.That(element1.value.name, Is.EqualTo(TEST_OBJ_NAME));
         }
 
         [Test]
-        public void TestDecrementingSizeRemovesElementsFromTheEnd()
+        public void TestCorrectObjectFieldRemovedOnRemoveElement()
         {
-            SetupMockToReturnObjects(5);
-            SetupFirstElement();
-            sizeField.value = 5;
+            SetupMockToReturnObjects(2);
+            listField.AddElement();
+            listField.AddElement();
 
-            sizeField.value = 3;
+            listField.RemoveElement();
 
-            var lastElement = listField.Query<ObjectField>().Last();
-            Assert.That(lastElement.name, Is.EqualTo("element2"));
+            var foundElements = listField.Query<ObjectField>().ToList();
+            Assert.That(foundElements.Count, Is.EqualTo(1));
+            Assert.That(foundElements[0].name, Is.EqualTo("element0"));
         }
 
         [Test]
-        public void TestSettingNegativeSizeDoesNotChangeValue()
+        public void TestLastValueRemovedOnRemoveElement()
         {
-            const int INITIAL_SIZE = 2;
-            SetupMockToReturnObjects(INITIAL_SIZE);
-            sizeField.value = INITIAL_SIZE;
+            const int INITIAL_OBJECTS_COUNT = 2;
+            SetupMockToReturnObjects(2);
+            for (int i = 0; i < INITIAL_OBJECTS_COUNT; i++)
+            {
+                listField.AddElement();
+                SetTestElementAtId(i);
+            }
 
-            sizeField.value = -1;
+            listField.RemoveElement();
 
-            Assert.That(sizeField.value, Is.EqualTo(INITIAL_SIZE));
-            var elements = listField.Query<ObjectField>().ToList();
-            Assert.That(elements.Count, Is.EqualTo(INITIAL_SIZE));
+            Assert.That(listField.value.Count, Is.EqualTo(1));
+            Assert.That(listField.value[0].name, Is.EqualTo($"{TEST_OBJ_NAME}0"));
+        }
+
+        private void SetTestElementAtId(int id)
+        {
+            var testObject = ScriptableObject.CreateInstance<ScriptableObject>();
+            testObject.name = $"{TEST_OBJ_NAME}{id}";
+            listField.value[id] = testObject;
         }
 
         [Test]
-        public void TestValuePropertyUpdatedOnAdd()
+        public void TestCorrectListValueUpdatedOnUpdateValueAt()
         {
-            const int VALUES_TO_ADD = 2;
-            SetupMockToReturnObjects(VALUES_TO_ADD);
-            sizeField.value = VALUES_TO_ADD;
+            SetupMockToReturnObjects(2);
+            listField.AddElement();
+            listField.AddElement();
+            var testObject = ScriptableObject.CreateInstance<ScriptableObject>();
+            testObject.name = TEST_OBJ_NAME;
+            var element1 = listField.Query<ObjectField>("element1").First();
+            element1.value = testObject;
 
-            var value = listField.value;
-            Assert.That(value.Count, Is.EqualTo(VALUES_TO_ADD));
-        }
+            listField.UpdateValueAt(1);
 
-        [Test]
-        public void TestValuePropertyUpdatedOnRemove()
-        {
-            const int INITIAL_SIZE = 3;
-            const int TARGET_SIZE = 2;
-            SetupMockToReturnObjects(INITIAL_SIZE);
-            sizeField.value = INITIAL_SIZE;
-            sizeField.value = TARGET_SIZE;
-
-            var value = listField.value;
-            Assert.That(value.Count, Is.EqualTo(TARGET_SIZE));
+            Assert.That(listField.value[1].name, Is.EqualTo(TEST_OBJ_NAME));
         }
     }
 }
