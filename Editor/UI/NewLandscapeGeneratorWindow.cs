8642a7bb8366761fcd92239e7932b2155c56a949
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-24 22:45:07 +0100

    Implemented Create Generator button validation.

--- Editor/UI/NewLandscapeGeneratorWindow.cs
+++ Editor/UI/NewLandscapeGeneratorWindow.cs
@@ -8,24 +8,51 @@ namespace ProceduralToolkit.UI
     public class NewLandscapeGeneratorWindow : EditorWindow
     {
         [MenuItem("Window/Procedural Toolkit/New Landscape Generator")]
-        public static void ShowNewLandscapeGeneratorWindow()
+        public static void ShowWindow()
         {
-            GetWindow<NewLandscapeGeneratorWindow>();
+            GetWindow<NewLandscapeGeneratorWindow>(title: "New Landscape Generator",
+                                                   utility: true);
         }
 
-        public InspectorElement ParametersElement
-        {
-            set
-            {
-                var inspectorRoot = rootVisualElement.Query<VisualElement>("inspectorRoot").First();
-                inspectorRoot.Add(value);
-            }
-        }
+        public BaseShapeGeneratorSettings baseShape;
 
         public void CreateGUI()
+        {
+            LoadLayout();
+            AddInspector();
+        }
+
+        private void LoadLayout()
         {
             var uxml = Resources.Load<VisualTreeAsset>("Layouts/new-landscape-generator-window");
             uxml.CloneTree(rootVisualElement);
         }
+
+        private void AddInspector()
+        {
+            var inspectorRoot = rootVisualElement.Query<VisualElement>("inspectorRoot").First();
+            inspectorRoot.Add(new InspectorElement(this));
+        }
+
+        public void OnFocus()
+        {
+            UpdateButtonActiveState();
+        }
+
+        public void OnValidate()
+        {
+            UpdateButtonActiveState();
+        }
+
+        private void UpdateButtonActiveState()
+        {
+            CreateButton.SetEnabled(IsFormValidated);
+        }
+
+        private Button CreateButton =>
+            rootVisualElement.Query<Button>("createGenerator").First();
+
+        private bool IsFormValidated =>
+            baseShape != null;
     }
 }
--- Tests/Editor/UITests/NewLandscapeGeneratorWindowTest.cs
+++ Tests/Editor/UITests/NewLandscapeGeneratorWindowTest.cs
@@ -5,13 +5,21 @@ using UnityEngine.TestTools;
 using ProceduralToolkit.UI;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
+using UnityEngine;
 
 namespace ProceduralToolkit.EditorTests.UITests
 {
     public class NewLandscapeGeneratorWindowTest
     {
+        internal class TestBaseShapeGeneratorSettings : BaseShapeGeneratorSettings
+        {
+            protected override IGenerator CreateGenerator()
+            {
+                return null;
+            }
+        }
+
         private NewLandscapeGeneratorWindow window;
-        private ObjectField baseShapeField;
 
         private VisualElement Root =>
             window.rootVisualElement;
@@ -22,62 +30,47 @@ namespace ProceduralToolkit.EditorTests.UITests
         private InspectorElement ParametersElement =>
             Root.Query<InspectorElement>().First();
 
-        [UnitySetUp]
-        public IEnumerator SetUp()
+        [SetUp]
+        public void SetUp()
         {
             window = EditorWindow.CreateWindow<NewLandscapeGeneratorWindow>();
-            window.ParametersElement = CreateInspector();
-            yield return null;
         }
 
-        private InspectorElement CreateInspector()
-        {
-            var inspector = new InspectorElement();
-            inspector.Add(BaseShapeField);
-            return inspector;
-        }
-
-        private ObjectField BaseShapeField
-        {
-            get
-            {
-                if (baseShapeField == null)
-                {
-                    baseShapeField = new ObjectField()
-                    {
-                        objectType = typeof(BaseShapeGeneratorSettings)
-                    };
-                }
-                return baseShapeField;
-            }
-        }
-
-        [UnityTearDown]
-        public IEnumerator TearDown()
+        [TearDown]
+        public void TearDown()
         {
             window.Close();
-            yield return null;
         }
 
-        [UnityTest]
-        public IEnumerator TestHasCreateGeneratorButton()
+        [Test]
+        public void TestHasCreateGeneratorButton()
         {
             Assert.That(CreateButton, Is.Not.Null);
-            yield return null;
         }
 
-        [UnityTest]
-        public IEnumerator TestInspectorElementAdded()
+        [Test]
+        public void TestInspectorElementAdded()
         {
             Assert.That(ParametersElement, Is.Not.Null);
-            yield return null;
         }
 
-        [UnityTest]
-        public IEnumerator TestCreateGeneratorIsNotActiveWhenBaseShapeIsNull()
+        [Test]
+        public void TestCreateGeneratorIsNotActiveWhenBaseShapeIsNull()
         {
             Assert.That(CreateButton.enabledSelf, Is.False);
-            yield return null;
+        }
+
+        [Test]
+        public void TestCreateGeneratorIsActiveWhenBaseShapeIsNotNull()
+        {
+            var testObject = ScriptableObject.CreateInstance<TestBaseShapeGeneratorSettings>();
+            window.baseShape = testObject;
+
+            window.OnValidate();
+
+            Assert.That(CreateButton.enabledSelf, Is.True);
+
+            Object.DestroyImmediate(testObject);
         }
     }
 }
