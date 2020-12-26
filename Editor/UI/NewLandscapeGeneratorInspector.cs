85f3e13a3782e79e205dacb4ad82c632cd1cc064
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-26 18:28:41 +0100

    Implemented inspector for new landscape generator

---
+++ Editor/UI/NewLandscapeGeneratorInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace ProceduralToolkit.UI
+{
+    [CustomEditor(typeof(NewLandscapeGeneratorWindow))]
+    public class NewLandscapeGeneratorInspector : Editor
+    {
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = new VisualElement();
+            var baseShapeField = new PropertyField()
+            {
+                name = "baseShape",
+                bindingPath = "baseShape"
+            };
+            root.Add(baseShapeField);
+            return root;
+        }
+    }
+}
---
+++ Tests/Editor/UITests/NewLandscapeGeneratorInspectorTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.UI;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace ProceduralToolkit.EditorTests.UITests
+{
+    public class NewLandscapeGeneratorInspectorTest
+    {
+        private NewLandscapeGeneratorWindow window;
+        private InspectorElement inspector;
+
+        private const string BASE_SHAPE_FIELD_NAME = "baseShape";
+
+        [SetUp]
+        public void SetUp()
+        {
+            window = EditorWindow.CreateWindow<NewLandscapeGeneratorWindow>();
+            inspector = new InspectorElement(window);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            window.Close();
+        }
+
+        [Test]
+        public void TestHasBaseShapeField()
+        {
+            var baseShapeField = inspector.Query<PropertyField>(BASE_SHAPE_FIELD_NAME).First();
+            Assert.That(baseShapeField, Is.Not.Null);
+            Assert.That(baseShapeField.bindingPath, Is.EqualTo(BASE_SHAPE_FIELD_NAME));
+        }
+    }
+}
