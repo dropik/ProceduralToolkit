bd5c0529f8365a8c3c150fb5278d1fc199cd7313
Daniil Ryzhkov <drop.sovet@gmail.com>
2020-12-27 01:11:43 +0100

    Isolated better utils tests

--- Tests/Editor/Utils/Tests/ButtonClickerTest.cs
+++ Tests/Editor/Utils/Tests/ButtonClickerTest.cs
@@ -5,7 +5,7 @@ using UnityEditor;
 using UnityEngine.TestTools;
 using UnityEngine.UIElements;
 
-namespace ProceduralToolkit.EditorTests.UtilsTests
+namespace ProceduralToolkit.UtilsTests
 {
     public class ButtonClickerTest
     {
