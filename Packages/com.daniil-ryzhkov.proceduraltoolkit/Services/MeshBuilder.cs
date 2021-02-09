f7cd8b85b7fe1aa83244943f225dd20ea776d982
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-09 23:39:53 +0100

    Cleaned up MeshBuilder

--- Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/MeshBuilder.cs
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/MeshBuilder.cs
@@ -9,7 +9,10 @@ namespace ProceduralToolkit.Services
         private readonly Func<IEnumerable<Vector3>> verticesProvider;
         private readonly Func<IEnumerable<int>> trianglesProvider;
 
-        private Mesh resultingMesh;
+        private IEnumerator<Vector3> verticesEnumerator;
+        private IEnumerator<int> trianglesEnumerator;
+        private List<Vector3> verticesList;
+        private List<int> trianglesList;
 
         public MeshBuilder(Func<IEnumerable<Vector3>> verticesProvider, Func<IEnumerable<int>> trianglesProvider)
         {
@@ -19,23 +22,75 @@ namespace ProceduralToolkit.Services
 
         public Mesh Build()
         {
-            resultingMesh = new Mesh();
-            var verticesEnumerator = verticesProvider.Invoke().GetEnumerator();
-            var trianglesEnumerator = trianglesProvider.Invoke().GetEnumerator();
-            var verticesList = new List<Vector3>();
-            var trianglesList = new List<int>();
+            var mesh = new Mesh();
+            BuildToMesh(mesh);
+            return mesh;
+        }
+
+        private void BuildToMesh(Mesh mesh)
+        {
+            SaveEnumeratorsToMesh(mesh);
+            mesh.RecalculateNormals();
+        }
+
+        private void SaveEnumeratorsToMesh(Mesh mesh)
+        {
+            ProcessEnumeratorsToLists();
+            SaveListsToMesh(mesh);
+            ClearLists();
+        }
+
+        private void ProcessEnumeratorsToLists()
+        {
+            UpdateEnumerators();
+            InitLists();
+            FillLists();
+        }
+
+        private void SaveListsToMesh(Mesh mesh)
+        {
+            mesh.vertices = verticesList.ToArray();
+            mesh.triangles = trianglesList.ToArray();
+        }
+
+        private void ClearLists()
+        {
+            verticesList.Clear();
+            trianglesList.Clear();
+        }
+
+        private void UpdateEnumerators()
+        {
+            verticesEnumerator = verticesProvider.Invoke().GetEnumerator();
+            trianglesEnumerator = trianglesProvider.Invoke().GetEnumerator();
+        }
+
+        private void InitLists()
+        {
+            verticesList = new List<Vector3>();
+            trianglesList = new List<int>();
+        }
+
+        private void FillLists()
+        {
             while (trianglesEnumerator.MoveNext())
             {
-                if (verticesEnumerator.MoveNext())
-                {
-                    verticesList.Add(verticesEnumerator.Current);
-                }
-                trianglesList.Add(trianglesEnumerator.Current);
+                HandleOneIndex();
             }
-            resultingMesh.vertices = verticesList.ToArray();
-            resultingMesh.triangles = trianglesList.ToArray();
-            resultingMesh.RecalculateNormals();
-            return resultingMesh;
+        }
+
+        private void HandleOneIndex()
+        {
+            TryAddVertex();
+            trianglesList.Add(trianglesEnumerator.Current);
+        }
+
+        private void TryAddVertex()
+        {
+            if (verticesEnumerator.MoveNext())
+            {
+                verticesList.Add(verticesEnumerator.Current);
+            }
         }
     }
 }
