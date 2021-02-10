19a042dbdad98a5cd32b38cef22708190373eb0f
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-10 16:51:01 +0100

    Implemented SquaresToIndicesConverter

--- Assets/EditorTests/Unit/Services/Generators/SquaresToIndicesConverterTests.cs
+++ Assets/EditorTests/Unit/Services/Generators/SquaresToIndicesConverterTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using ProceduralToolkit.Models;
 using ProceduralToolkit.Services.Generators;
 
@@ -19,6 +20,35 @@ namespace ProceduralToolkit.EditorTests.Unit.Services.Generators
                 0, 1, 2,
                 0, 2, 3
             };
+            TestConverter(squares, expectedIndices);
+        }
+
+        [Test]
+        public void TestConvertingMultipleSquares()
+        {
+            var squares = new Square[]
+            {
+                new Square(0, 1, 4, 3),
+                new Square(1, 2, 5, 4),
+                new Square(3, 4, 7, 6),
+                new Square(4, 5, 8, 7)
+            };
+            var expectedIndices = new int[]
+            {
+                0, 1, 4,
+                0, 4, 3,
+                1, 2, 5,
+                1, 5, 4,
+                3, 4, 7,
+                3, 7, 6,
+                4, 5, 8,
+                4, 8, 7
+            };
+            TestConverter(squares, expectedIndices);
+        }
+
+        private void TestConverter(IEnumerable<Square> squares, IEnumerable<int> expectedIndices)
+        {
             var converter = new SquaresToIndicesConverter(squares);
             CollectionAssert.AreEqual(expectedIndices, converter.Indices);
         }
