63e20c6248956a29349f8092649cf0d079e5daa3
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-17 19:24:29 +0100

    Implemented StoreFirst

---
+++ Assets/EditorTests/Unit/Services/Generators/StoreFirstTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using NUnit.Framework;
+using ProceduralToolkit.Models;
+using ProceduralToolkit.Services.Generators;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators
+{
+    [Category("Unit")]
+    public class StoreFirstTests
+    {
+        private Vector3[] inputVertices = new Vector3[]
+        {
+            new Vector3(1, 0, 0),
+            new Vector3(2, 0, 0)
+        };
+        private DiamondContext context;
+        private Mock<IState> mockNextState;
+        private StoreFirst storeFirst;
+
+        [SetUp]
+        public void Setup()
+        {
+            mockNextState = new Mock<IState>();
+            mockNextState.Setup(m => m.Equals(It.Is<string>(s => s == "mock"))).Returns(true);
+            context = new DiamondContext()
+            {
+                Length = 2
+            };
+            var enumerator = ((IEnumerable<Vector3>)inputVertices).GetEnumerator();
+            storeFirst = new StoreFirst(enumerator, context)
+            {
+                NextState = mockNextState.Object
+            };
+        }
+
+        [Test]
+        public void TestCurrentSetAsInputVertex()
+        {
+            storeFirst.MoveNext();
+            Assert.That(context.Current, Is.EqualTo(inputVertices[0]));
+        }
+
+        [Test]
+        public void TestNextStateSet()
+        {
+            storeFirst.MoveNext();
+            Assert.That(context.State.Equals("mock"));
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/StoreFirst.cs
@@ -0,0 +1,27 @@
+﻿using ProceduralToolkit.Models;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators
+{
+    public class StoreFirst : IState
+    {
+        private readonly IEnumerator<Vector3> inputVerticesEnumerator;
+        private readonly DiamondContext context;
+
+        public StoreFirst(IEnumerator<Vector3> inputVerticesEnumerator, DiamondContext context)
+        {
+            this.inputVerticesEnumerator = inputVerticesEnumerator;
+            this.context = context;
+        }
+
+        public IState NextState { get; set; }
+
+        public void MoveNext()
+        {
+            inputVerticesEnumerator.MoveNext();
+            context.Current = inputVerticesEnumerator.Current;
+            context.State = NextState;
+        }
+    }
+}
