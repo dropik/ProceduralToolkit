04a3de9089dc4052692f04e8b703765b374b6889
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-20 14:43:23 +0200

    Implemented RowDuplicator

---
+++ Assets/EditorTests/Unit/Services/Generators/RowDuplicatorTests.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Services.Generators;
+using System.Linq;
+using UnityEngine;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators
+{
+    [Category("Unit")]
+    public class RowDuplicatorTests
+    {
+        [Test]
+        public void TestRowsAreDuplicated()
+        {
+            var row1 = new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0)
+            };
+            var row2 = new Vector3[]
+            {
+                new Vector3(0, 0, 1),
+                new Vector3(1, 0, 1)
+            };
+            var row3 = new Vector3[]
+            {
+                new Vector3(0, 0, 2),
+                new Vector3(1, 0, 2)
+            };
+            var inputVertices = row1.Concat(row2).Concat(row3);
+            var expectedVertices = row1.Concat(row2).Concat(row2).Concat(row3).Concat(row3);
+            var duplicator = new RowDuplicator()
+            {
+                InputVertices = inputVertices,
+                ColumnsInRow = 2
+            };
+            CollectionAssert.AreEqual(expectedVertices, duplicator.OutputVertices);
+        }
+
+        [Test]
+        public void TestOnInputVerticesNotSet()
+        {
+            var duplicator = new RowDuplicator()
+            {
+                ColumnsInRow = 2
+            };
+            var expectedVertices = new Vector3[0];
+            CollectionAssert.AreEqual(expectedVertices, duplicator.OutputVertices);
+        }
+
+        [Test]
+        public void TestOnColumnsInRowNotSet()
+        {
+            var inputVertices = new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(2, 0, 0)
+            };
+            var duplicator = new RowDuplicator()
+            {
+                InputVertices = inputVertices
+            };
+            CollectionAssert.AreEqual(inputVertices, duplicator.OutputVertices);
+        }
+
+        [Test]
+        public void TestOnNegativeColumnsInRow()
+        {
+            var inputVertices = new Vector3[]
+           {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(2, 0, 0)
+           };
+            var duplicator = new RowDuplicator()
+            {
+                InputVertices = inputVertices,
+                ColumnsInRow = -2
+            };
+            CollectionAssert.AreEqual(inputVertices, duplicator.OutputVertices);
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/RowDuplicator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators
+{
+    public class RowDuplicator
+    {
+        public IEnumerable<Vector3> InputVertices
+        {
+            get => inputVertices ?? (new Vector3[0]);
+            set => inputVertices = value;
+        }
+        private IEnumerable<Vector3> inputVertices;
+
+        public int ColumnsInRow
+        {
+            get => columnsInRow;
+            set => columnsInRow = value < 0 ? 0 : value;
+        }
+        private int columnsInRow;
+
+        public IEnumerable<Vector3> OutputVertices
+        {
+            get
+            {
+                var rowCounter = 0;
+                var columnCounter = 0;
+                var vertexCopies = new Vector3[ColumnsInRow];
+                foreach (var vertex in InputVertices)
+                {
+                    if ((rowCounter > 0) && (ColumnsInRow > 0))
+                    {
+                        vertexCopies[columnCounter] = vertex;
+                    }
+
+                    yield return vertex;
+                    columnCounter++;
+
+                    if (columnCounter >= ColumnsInRow)
+                    {
+                        columnCounter = 0;
+                        if (rowCounter > 0)
+                        {
+                            foreach (var copy in vertexCopies)
+                            {
+                                yield return copy;
+                            }
+                        }
+                        rowCounter++;
+                    }
+                }
+            }
+        }
+    }
+}
