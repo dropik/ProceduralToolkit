6707cf9b990735964464385ebdf5784a341b1777
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-22 22:17:45 +0200

    Implemented a new StoryCopy

---
+++ Assets/EditorTests/Unit/Services/Generators/FSM/NewStoreCopyTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Models.FSMContexts;
+using ProceduralToolkit.Services.Generators.FSM;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators.FSM
+{
+    public class NewStoreCopyTests : ReturnOriginalTests
+    {
+        protected override FSMContext CreateContext(int columns)
+        {
+            var context = base.CreateContext(columns);
+            context.RowDuplicatorContext = new RowDuplicatorContext(columns);
+            return context;
+        }
+
+        protected override BaseState GetReturnVertex(FSMSettings settings)
+        {
+            return new NewStoreCopy(settings);
+        }
+
+        [Test]
+        public void TestVertexCopyStored()
+        {
+            ReturnVertex.MoveNext(InputVertices[0]);
+            Assert.That(Settings.FSMContext.RowDuplicatorContext.VerticesCopies[0], Is.EqualTo(InputVertices[0]));
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/FSM/NewStoreCopy.cs
@@ -0,0 +1,16 @@
+﻿using ProceduralToolkit.Models.FSMContexts;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators.FSM
+{
+    public class NewStoreCopy : ReturnOriginal
+    {
+        public NewStoreCopy(FSMSettings settings) : base(settings) { }
+
+        protected override void PreprocessVertex(Vector3 vertex)
+        {
+            var context = Settings.FSMContext;
+            context.RowDuplicatorContext.VerticesCopies[context.Column] = vertex;
+        }
+    }
+}
