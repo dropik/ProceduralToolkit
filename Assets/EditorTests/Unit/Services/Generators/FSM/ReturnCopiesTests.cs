da7d7c6e663910e4915dc28adb97f05d364ffa5a
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-22 22:26:48 +0200

    Implemented ReturnCopies

---
+++ Assets/EditorTests/Unit/Services/Generators/FSM/ReturnCopiesTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Models.FSMContexts;
+using ProceduralToolkit.Services.Generators.FSM;
+using System.Linq;
+using UnityEngine;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators.FSM
+{
+    [Category("Unit")]
+    public class ReturnCopiesTests : BaseStateTests
+    {
+        protected override FSMContext CreateContext(int columns)
+        {
+            return new FSMContext(columns)
+            {
+                RowDuplicatorContext = new RowDuplicatorContext(columns)
+            };
+        }
+
+        protected override BaseState GetReturnVertex(FSMSettings settings)
+        {
+            return new ReturnCopies(settings);
+        }
+
+        [Test]
+        public void TestReturnedVertices()
+        {
+            var context = Settings.FSMContext;
+            context.Column = 1;
+            for (int i = 0; i < context.ColumnsInRow; i++)
+            {
+                context.RowDuplicatorContext.VerticesCopies[i] = new Vector3(0, 0, i);
+            }
+            var expectedVertices = new Vector3[] { InputVertices[1] }.Concat(context.RowDuplicatorContext.VerticesCopies);
+
+            CollectionAssert.AreEqual(expectedVertices, ReturnVertex.MoveNext(InputVertices[1]));
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/FSM/ReturnCopies.cs
@@ -0,0 +1,20 @@
+﻿using ProceduralToolkit.Models.FSMContexts;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators.FSM
+{
+    public class ReturnCopies : BaseState
+    {
+        public ReturnCopies(FSMSettings settings) : base(settings) { }
+
+        protected override IEnumerable<Vector3> GetResultVertices(Vector3 vertex)
+        {
+            yield return vertex;
+            foreach (var copy in Settings.FSMContext.RowDuplicatorContext.VerticesCopies)
+            {
+                yield return copy;
+            }
+        }
+    }
+}
