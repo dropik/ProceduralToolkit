bb5d603e7001d09e7b715830ff40cc2b7ce75129
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-26 22:05:05 +0200

    Implemented EnqueueVertex

---
+++ Assets/EditorTests/Unit/Services/Generators/FSM/EnqueueVertexTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Models.FSM;
+using ProceduralToolkit.Services.Generators.FSM;
+using UnityEngine;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators.FSM
+{
+    [Category("Unit")]
+    public class EnqueueVertexTests
+    {
+        [Test]
+        public void TestVertexEnqueued()
+        {
+            var input = new Vector3(1, 2, 3);
+            var context = new InvertorContext(3);
+            var enqueueVertex = new EnqueueVertex(context);
+
+            enqueueVertex.Process(input);
+            var result = context.Queue.Dequeue();
+            Assert.That(result, Is.EqualTo(input));
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/FSM/EnqueueVertex.cs
@@ -0,0 +1,20 @@
+﻿using ProceduralToolkit.Models.FSM;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators.FSM
+{
+    public class EnqueueVertex : IVertexPreprocessor
+    {
+        private readonly InvertorContext context;
+
+        public EnqueueVertex(InvertorContext context)
+        {
+            this.context = context;
+        }
+
+        public void Process(Vector3 vertex)
+        {
+            context.Queue.Enqueue(vertex);
+        }
+    }
+}
