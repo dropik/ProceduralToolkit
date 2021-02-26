71fbd7fa86ec9f851da11d153aae6ef17666c500
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-02-26 22:31:31 +0200

    Implemented Invertor

---
+++ Assets/EditorTests/IT/InvertorIT.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Services.Generators;
+using System.Linq;
+using UnityEngine;
+
+namespace ProceduralToolkit.EditorTests.IT
+{
+    [Category("IT")]
+    public class InvertorIT
+    {
+        [Test]
+        public void TestRowsInverted()
+        {
+            var row1 = new Vector3[]
+            {
+                new Vector3(0, 0, 0),
+                new Vector3(1, 0, 0),
+                new Vector3(2, 0, 0),
+                new Vector3(3, 0, 0)
+            };
+            var row2 = new Vector3[]
+            {
+                new Vector3(0, 0, 1),
+                new Vector3(1, 0, 1),
+                new Vector3(2, 0, 1)
+            };
+            var row3 = new Vector3[]
+            {
+                new Vector3(0, 0, 2),
+                new Vector3(1, 0, 2),
+                new Vector3(2, 0, 2),
+                new Vector3(3, 0, 2)
+            };
+            var row4 = new Vector3[]
+            {
+                new Vector3(0, 0, 3),
+                new Vector3(1, 0, 3),
+                new Vector3(2, 0, 3),
+            };
+            var row5 = new Vector3[]
+            {
+                new Vector3(0, 0, 4),
+                new Vector3(1, 0, 4),
+                new Vector3(2, 0, 4),
+                new Vector3(3, 0, 4)
+            };
+            var input = row1.Concat(row2).Concat(row3).Concat(row4).Concat(row5).Concat(new Vector3[3]);
+            var expectedOuptut = row1.Concat(row3).Concat(row2).Concat(row5).Concat(row4);
+
+            var invertor = InvertorFactory.Create();
+            invertor.InputVertices = input;
+            invertor.ColumnsInRow = 4;
+
+            CollectionAssert.AreEqual(expectedOuptut, invertor.OutputVertices);
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/InvertorFactory.cs
@@ -0,0 +1,49 @@
+﻿using ProceduralToolkit.Models.FSM;
+using ProceduralToolkit.Services.Generators.FSM;
+using System.Collections.Generic;
+
+namespace ProceduralToolkit.Services.Generators
+{
+    public static class InvertorFactory
+    {
+        public static FSMBasedGenerator Create() => new FSMBasedGenerator(columns =>
+        {
+            var context = new InvertorContext(columns);
+            var machine = new Machine(new Dictionary<string, IState>(), () => new StateBuilder(context));
+
+            bool OriginalRowEnd() => context.Column >= columns;
+            bool DiamondRowEnd() => context.Column >= columns - 1;
+
+            machine.AddState("OutputOriginal1", builder =>
+            {
+                builder.SetDefaultState("OutputOriginal1")
+                       .On(OriginalRowEnd).SetNext("EnqueueVertex");
+            });
+
+            machine.AddState("EnqueueVertex", builder =>
+            {
+                builder.ConfigureOutput(new OutputSkip())
+                       .ConfigurePreprocessor(new EnqueueVertex(context))
+                       .SetDefaultState("EnqueueVertex")
+                       .On(DiamondRowEnd).SetNext("OutputOriginal2");
+            });
+
+            machine.AddState("OutputOriginal2", builder =>
+            {
+                builder.SetDefaultState("OutputOriginal2")
+                       .On(OriginalRowEnd).SetNext("OutputDequeued");
+            });
+
+            machine.AddState("OutputDequeued", builder =>
+            {
+                builder.ConfigureOutput(new OutputDequeued(context))
+                       .ConfigurePreprocessor(new EnqueueVertex(context))
+                       .SetDefaultState("OutputDequeued")
+                       .On(DiamondRowEnd).SetNext("OutputOriginal2");
+            });
+
+            machine.SetState("OutputOriginal1");
+            return machine;
+        });
+    }
+}
