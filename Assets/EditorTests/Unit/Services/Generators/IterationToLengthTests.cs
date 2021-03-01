ed6373a80746b25256b2eebf53c314c221d18ec9
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-01 19:56:23 +0200

    Implemented IterationToLength

---
+++ Assets/EditorTests/Unit/Services/Generators/IterationToLengthTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Services.Generators;
+
+namespace ProceduralToolkit.EditorTests.Unit.Services.Generators
+{
+    [Category("Unit")]
+    public class IterationToLengthTests
+    {
+        [Test]
+        [TestCase(0, 2)]
+        [TestCase(2, 5)]
+        public void TestCalculatedLength(int iteration, int expectedLength)
+        {
+            var iterationToLength = new IterationToLength()
+            {
+                Iteration = iteration
+            };
+            Assert.That(iterationToLength.Length, Is.EqualTo(expectedLength));
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/IterationToLength.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators
+{
+    public class IterationToLength
+    {
+        public int Iteration { get; set; }
+        public int Length => (int)Mathf.Pow(2, Iteration) + 1;
+    }
+}
