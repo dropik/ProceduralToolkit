1d6b479249f8d84492da5299c52c70411e0f8a12
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-01 14:27:12 +0200

    Implemented OutputMeanHeight

---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/FSM/OutputMeanHeight.cs
@@ -0,0 +1,23 @@
+﻿using ProceduralToolkit.Models.FSM;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.Services.Generators.FSM
+{
+    public class OutputMeanHeight : IStateOutput
+    {
+        private readonly AdderContext context;
+
+        public OutputMeanHeight(AdderContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Vector3> GetOutputFor(Vector3 vertex)
+        {
+            var height = context.Heights.Dequeue();
+            var mean = (vertex.y + height) / 4;
+            yield return new Vector3(vertex.x, mean, vertex.z);
+        }
+    }
+}
