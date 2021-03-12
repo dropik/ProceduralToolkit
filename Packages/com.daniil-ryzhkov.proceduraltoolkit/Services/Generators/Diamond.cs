7c707c64a0132c645ced2bc2334e6a9354ac0ac3
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-12 15:19:52 +0200

    Inserted squares into array

--- Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/Diamond.cs
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Services/Generators/Diamond.cs
@@ -8,7 +8,7 @@ namespace ProceduralToolkit.Services.Generators
         private readonly Vector3[] input;
         private readonly int iteration;
         private readonly DSASettings settings;
-        private readonly int verticesInRow;
+        private readonly int length;
         private readonly Vector3 xzShift;
 
         public Diamond(Vector3[] input, int iteration, DSASettings settings)
@@ -17,9 +17,9 @@ namespace ProceduralToolkit.Services.Generators
             this.iteration = iteration;
             this.settings = settings;
 
-            verticesInRow = (int)Mathf.Pow(2, iteration) + 1;
+            length = (int)Mathf.Sqrt(input.Length);
 
-            xzShift = (input[OriginalIndex(1, 1)] - input[0]) / 2f;
+            xzShift = (input[2 * (length + 1)] - input[0]) / 2f;
             xzShift.y = 0;
         }
 
@@ -29,14 +29,14 @@ namespace ProceduralToolkit.Services.Generators
             {
                 Random.InitState(settings.Seed);
 
-                for (int i = 1; i < 2 * verticesInRow - 1; i += 2)
+                for (int i = 1; i < length; i += 2)
                 {
-                    for (int j = 0; j < verticesInRow - 1; j++)
+                    for (int j = 1; j < length; j += 2)
                     {
-                        input[(i / 2) * (2 * verticesInRow - 1) + verticesInRow + j] = input[OriginalIndex(i / 2, j)] + xzShift;
-                        input[(i / 2) * (2 * verticesInRow - 1) + verticesInRow + j].y += (input[OriginalIndex(i / 2, j + 1)].y + input[OriginalIndex((i + 1) / 2, j)].y + input[OriginalIndex((i + 1) / 2, j + 1)].y);
-                        input[(i / 2) * (2 * verticesInRow - 1) + verticesInRow + j].y /= 4f;
-                        input[(i / 2) * (2 * verticesInRow - 1) + verticesInRow + j].y += GetElevation();
+                        input[i * length + j] = input[(i - 1) * length + j - 1] + xzShift;
+                        input[i * length + j].y += (input[(i - 1) * length + j + 1].y + input[(i + 1) * length + j - 1].y + input[(i + 1) * length + j + 1].y);
+                        input[i * length + j].y /= 4f;
+                        input[i * length + j].y += GetElevation();
                     }
                 }
                 return input;
@@ -48,8 +48,5 @@ namespace ProceduralToolkit.Services.Generators
             var magnitude = Mathf.Pow(2, -1 * iteration * settings.Hardness) * settings.Magnitude;
             return Random.Range(-magnitude, magnitude);
         }
-
-        private int Original(int row) => row * (2 * verticesInRow - 1);
-        private int OriginalIndex(int row, int column) => Original(row) + column;
     }
 }
