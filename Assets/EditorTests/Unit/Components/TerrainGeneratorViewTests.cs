113c46115d0b4d1ecba291a5fe43ecbe9a58d62a
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-19 21:25:59 +0200

    Implemented TerrainGeneratorView

---
+++ Assets/EditorTests/Unit/Components/TerrainGeneratorViewTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using ProceduralToolkit.Models;
+using ProceduralToolkit.Services.DI;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProceduralToolkit.Components
+{
+    [Category("Unit")]
+    public class TerrainGeneratorViewTests
+    {
+        private readonly float[,] heights = new float[RESOLUTION, RESOLUTION];
+
+        private GameObject gameObject;
+        private Terrain terrain;
+        private TerrainData terrainData;
+        private TerrainGeneratorView view;
+
+        private const int RESOLUTION = 33;
+
+        [SetUp]
+        public void Setup()
+        {
+            gameObject = new GameObject();
+
+            terrain = gameObject.AddComponent<Terrain>();
+            terrainData = new TerrainData();
+            terrain.terrainData = terrainData;
+            terrainData.heightmapResolution = RESOLUTION;
+            terrainData.SetHeights(0, 0, heights);
+
+            view = gameObject.AddComponent<TerrainGeneratorView>();
+            var services = ServiceContainerFactory.Create();
+            services.AddSingleton(terrain);
+            services.AddSingleton(new DsaSettings { Magnitude = 300 });
+            services.InjectServicesTo(view);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Object.DestroyImmediate(gameObject);
+        }
+
+        [Test]
+        public void TestNothingHappensOnNullNewMesh()
+        {
+            view.Update();
+            CollectionAssert.AreEqual(heights, terrainData.GetHeights(0, 0, RESOLUTION, RESOLUTION));
+        }
+
+        [Test]
+        public void TestTerrainHeightsUpdatedOnNewMesh()
+        {
+            var vertices = new Vector3[RESOLUTION * RESOLUTION];
+            vertices[3] = new Vector3(0, 200, 0);
+            vertices[10] = new Vector3(0, 24, 0);
+            vertices[50] = new Vector3(0, -100, 0);
+            vertices[123] = new Vector3(0, 300, 0);
+
+            var mesh = new Mesh()
+            {
+                vertices = vertices
+            };
+
+            var expectedHeights = new float[RESOLUTION, RESOLUTION];
+            for (int i = 0; i < RESOLUTION; i++)
+            {
+                for (int j = 0; j < RESOLUTION; j++)
+                {
+                    expectedHeights[i, j] = 0.5f;
+                }
+            }
+            expectedHeights[0, 3] = 0.8333333333f;
+            expectedHeights[0, 10] = 0.54f;
+            expectedHeights[1, 17] = 0.3333333333f;
+            expectedHeights[3, 24] = 1;
+
+            view.NewMesh = mesh;
+            view.Update();
+
+            CollectionAssert.AreEqual(expectedHeights,
+                                      terrainData.GetHeights(0, 0, RESOLUTION, RESOLUTION),
+                                      Comparer<float>.Create((x, y) => Mathf.Abs(x - y) >= 0.0001f ? 1 : 0));
+        }
+
+        [Test]
+        public void TestNewMeshNulledAfterUpdate()
+        {
+            view.NewMesh = new Mesh()
+            {
+                vertices = new Vector3[RESOLUTION * RESOLUTION]
+            };
+            view.Update();
+            Assert.That(view.NewMesh, Is.Null);
+        }
+    }
+}
---
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Components/TerrainGeneratorView.cs
@@ -0,0 +1,40 @@
+﻿using ProceduralToolkit.Models;
+using ProceduralToolkit.Services.DI;
+using UnityEngine;
+
+namespace ProceduralToolkit.Components
+{
+    public class TerrainGeneratorView : MonoBehaviour, IGeneratorView
+    {
+        [Service]
+        private readonly Terrain terrain;
+        [Service]
+        private readonly DsaSettings settings;
+
+        public Mesh NewMesh { get; set; }
+
+        public void Update()
+        {
+            if (NewMesh != null)
+            {
+                var terrainData = terrain.terrainData;
+                var resolution = terrainData.heightmapResolution;
+                var index = 0;
+                var heights = new float[resolution, resolution];
+                var vertices = NewMesh.vertices;
+                var range = settings.Magnitude * 2;
+                for (int row = 0; row < resolution; row++)
+                {
+                    for (int column = 0; column < resolution; column++)
+                    {
+                        heights[row, column] = vertices[index].y / range + 0.5f;
+                        index++;
+                    }
+                }
+
+                terrainData.SetHeights(0, 0, heights);
+                NewMesh = null;
+            }
+        }
+    }
+}
