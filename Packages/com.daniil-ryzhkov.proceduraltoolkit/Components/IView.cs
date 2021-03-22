d73d0c2b8e118650f1a7dbd2c1f7c7f5e1d0f93a
Daniil Ryzhkov <drop.sovet@gmail.com>
2021-03-22 20:35:43 +0100

    Removed Update from IView

--- Packages/com.daniil-ryzhkov.proceduraltoolkit/Components/IView.cs
+++ Packages/com.daniil-ryzhkov.proceduraltoolkit/Components/IView.cs
@@ -1,12 +1,8 @@
-﻿using ProceduralToolkit.Models;
-using UnityEngine;
-
-namespace ProceduralToolkit.Components
+﻿namespace ProceduralToolkit.Components
 {
     public interface IView
     {
         void MarkDirty();
         bool IsDirty { get; }
-        void Update();
     }
 }
